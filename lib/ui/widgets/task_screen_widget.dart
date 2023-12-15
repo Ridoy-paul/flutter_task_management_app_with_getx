e78529a8f5160a4a8a734ffc9c919999549947f3
Ridoy paul <cse.ridoypaul@gmail.com>
2023-12-15 20:54:37 +0600

    task count summery updated

--- lib/ui/widgets/task_screen_widget.dart
+++ lib/ui/widgets/task_screen_widget.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
+import '../controllers/task_count_summery_controller.dart';
 import '../controllers/task_screen_controller.dart';
 import 'package:flutter_task_management_app/ui/style.dart';
 import 'package:get/get.dart';
 import 'profile_summery_card_widget.dart';
 import '../../data/models/task_count.dart';
-import '../../data/models/task_summery_count_summery_model.dart';
 import '../screens/add_new_task_screen.dart';
 import 'new_task_summery_widget.dart';
 import 'task_item_card_widget.dart';
-import '../../data/data_network_caller/network_caller.dart';
-import '../../data/data_network_caller/network_response.dart';
 import '../../data/utility/helpers.dart';
-import '../../data/utility/urls.dart';
 
 class TaskScreen extends StatefulWidget {
   final String taskType;
@@ -23,55 +20,9 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-  bool _getTaskCountSummeryInProgress = false;
 
-  //TaskListModel taskListModel = TaskListModel();
-  // TaskCountSummeryListModel taskCountSummeryListModel = TaskCountSummeryListModel();
   final TaskScreenController _taskScreenController = Get.find<TaskScreenController>();
-
-  // Future<void> getTaskList() async {
-  //   final response = _taskScreenController.getTaskList(widget.taskType);
-  //
-  //   print(response);
-  //   //taskListModel = TaskListModel.fromJson(response);
-  //
-  //   /*
-  //   setState(() {
-  //     _getTaskInProgress = true;
-  //   });
-  //
-  //   final NetworkResponse response = await NetworkCaller().getRequest(Urls.getTask(widget.taskType));
-  //
-  //   if (response.isSuccess) {
-  //     taskListModel = TaskListModel.fromJson(response.jsonResponse);
-  //   }
-  //
-  //   setState(() {
-  //     _getTaskInProgress = false;
-  //   });
-  //    */
-  // }
-
-  // Future<void> getTaskCountSummeryList() async {
-  //   _getTaskCountSummeryInProgress = true;
-  //
-  //   if (mounted) {
-  //     setState(() {});
-  //   }
-  //
-  //   final NetworkResponse response =
-  //   await NetworkCaller().getRequest(Urls.getTaskStatusCount);
-  //   if (response.isSuccess) {
-  //     taskCountSummeryListModel =
-  //         TaskCountSummeryListModel.fromJson(response.jsonResponse);
-  //   }
-  //
-  //   _getTaskCountSummeryInProgress = false;
-  //   if (mounted) {
-  //     setState(() {});
-  //   }
-  // }
-
+  final TaskCountSummeryController _taskCountSummeryController = Get.find<TaskCountSummeryController>();
 
   @override
   void initState() {
@@ -79,15 +30,9 @@ class _TaskScreenState extends State<TaskScreen> {
     _taskScreenController.getTaskList(widget.taskType);
 
     if (widget.taskType == "New") {
-      getTaskCountSummeryList();
+      _taskCountSummeryController.getTaskCountSummeryList();
     }
   }
-
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //   print("hello paul");
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -98,66 +43,71 @@ class _TaskScreenState extends State<TaskScreen> {
           children: [
             const ProfileSummery(),
             widget.taskType == 'New' ?
-            Visibility(
-              visible: _getTaskCountSummeryInProgress == false,
-              replacement: const LinearProgressIndicator(),
-              child: SizedBox(
-                height: 100,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: taskCountSummeryListModel.taskCountList
-                        ?.length ?? 0,
-                    itemBuilder: (context, index) {
-                      TaskCount taskCount = taskCountSummeryListModel
-                          .taskCountList![index];
-                      return FittedBox(
-                        child: SummeryCard(
-                          title: taskCount.sId ?? '',
-                          value: taskCount.sum.toString(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ) : Container(),
-            Expanded(
-              child: GetBuilder<TaskScreenController>(builder: (taskScreenController) {
-                return RefreshIndicator(
-                  onRefresh: ()=> taskScreenController.getTaskList(widget.taskType),
-                  child: Visibility(
-                    visible: !taskScreenController.getTaskScreenInProgress,
-                    replacement: circleProgressIndicatorShow(),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListView.builder(
-                        itemCount: taskScreenController.taskListModel.taskList?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          return TaskItemCard(
-                            task: taskScreenController.taskListModel.taskList![index],
-                            onStatusChange: () {
-                              taskScreenController.getTaskList(widget.taskType);
-                              if (widget.taskType == "New") {
-                                getTaskCountSummeryList();
-                              }
-                            },
-                            showProgress: (inProgress) {
-                              // setState(() {
-                              //   _getTaskInProgress = inProgress;
-                              // });
-                              // if (!inProgress) {
-                              //   showSnackMessage(context, "Task Status Updated.");
-                              // }
-                            },
-                          );
-                        },
-                      ),
+            GetBuilder<TaskCountSummeryController>(builder: (taskCountSummeryController) {
+              return Visibility(
+                visible: !taskCountSummeryController.getTaskCountSummeryInProgress,
+                replacement: const LinearProgressIndicator(),
+                child: SizedBox(
+                  height: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: taskCountSummeryController.taskCountSummeryListModel.taskCountList?.length ?? 0,
+                      itemBuilder: (context, index) {
+                        TaskCount taskCount = taskCountSummeryController.taskCountSummeryListModel.taskCountList![index];
+                        return FittedBox(
+                          child: SummeryCard(
+                            title: taskCount.sId ?? '',
+                            value: taskCount.sum.toString(),
+                          ),
+                        );
+                      },
                     ),
                   ),
-                );
-              }),
+                ),
+              );
+            }) : Container(),
+            Expanded(
+              child: GetBuilder<TaskScreenController>(
+                  builder: (taskScreenController) {
+                    return RefreshIndicator(
+                      onRefresh: () =>
+                          taskScreenController.getTaskList(widget.taskType),
+                      child: Visibility(
+                        visible: !taskScreenController.getTaskScreenInProgress,
+                        replacement: circleProgressIndicatorShow(),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListView.builder(
+                            itemCount: taskScreenController.taskListModel
+                                .taskList?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              return TaskItemCard(
+                                task: taskScreenController.taskListModel
+                                    .taskList![index],
+                                onStatusChange: () {
+                                  taskScreenController.getTaskList(
+                                      widget.taskType);
+                                  if (widget.taskType == "New") {
+                                    _taskCountSummeryController.getTaskCountSummeryList();
+                                  }
+                                },
+                                showProgress: (inProgress) {
+                                  // setState(() {
+                                  //   _getTaskInProgress = inProgress;
+                                  // });
+                                  // if (!inProgress) {
+                                  //   showSnackMessage(context, "Task Status Updated.");
+                                  // }
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
             ),
           ],
         ),
@@ -165,11 +115,9 @@ class _TaskScreenState extends State<TaskScreen> {
       floatingActionButton: widget.taskType == "New" ? FloatingActionButton(
         onPressed: () async {
           await Get.to(const AddNewTaskScreen());
-          //await Navigator.push(context, MaterialPageRoute(builder: (context) => const AddNewTaskScreen(),),);
-
           /// This code will run when coming back from add task screen.
           if (widget.taskType == "New") {
-            getTaskCountSummeryList();
+            _taskCountSummeryController.getTaskCountSummeryList();
           }
 
           _taskScreenController.getTaskList(widget.taskType);
